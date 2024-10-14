4a5fd1cf47da57acd1319c949e0196476363994e
Manan Gandhi <ardumanan@gmail.com>
2024-10-14 20:55:12 +0530

    [code-runner] Better error handling

--- code-runner/executor.py
+++ code-runner/executor.py
@@ -27,28 +27,36 @@ def get_exec_command(filename):
 with open("input.txt", "r") as f:
     [filename, *input_data] = f.read().strip().split("\n---")
 
-compile_process = subprocess.run(
-    get_compile_command(filename),
-    shell=True,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-)
-if compile_process.stderr:
-    print(compile_process.stderr.decode(), file=sys.stderr)
-    exit()
-else:
-    print(compile_process.stdout.decode())
-
-for data in input_data:
-    execute_process = subprocess.run(
-        get_exec_command(filename),
+try:
+    compile_process = subprocess.run(
+        get_compile_command(filename),
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        input=data.strip().encode(),
     )
-    if execute_process.stderr:
-        print(execute_process.stderr.decode(), file=sys.stderr)
+    if compile_process.stderr:
+        print(compile_process.stderr.decode(), file=sys.stderr)
+        exit()
     else:
-        print(execute_process.stdout.decode())
-    print("---")
+        print(compile_process.stdout.decode())
+except:
+    print("Could not execute code", file=sys.stderr)
+    exit()
+
+for data in input_data:
+    try:
+        execute_process = subprocess.run(
+            get_exec_command(filename),
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            input=data.strip().encode(),
+        )
+        if execute_process.stderr:
+            print(execute_process.stderr.decode(), file=sys.stderr)
+        else:
+            print(execute_process.stdout.decode())
+        print("---")
+    except:
+        print("Could not execute code", file=sys.stderr)
+        exit()
