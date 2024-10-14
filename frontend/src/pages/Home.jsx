619a1de6fe0b34c10384b68e921fd090a388ff63
Manan Gandhi <ardumanan@gmail.com>
2024-10-14 18:29:38 +0530

    [frontend] Self closing span tag

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
             <div className="container px-4 md:px-6">
                 <div className="flex flex-col items-center space-y-4 text-center">
                     <div className="space-y-2">
-                        <span className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none type-home-heading"></span>
+                        <span className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none type-home-heading" />
                         <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                             Sharpen your coding skills with our vast collection
                             of programming challenges. Practice, learn, and
