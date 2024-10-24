bfab831b3cf7db712a28b467bce6a6e844ba69cb
Manan Gandhi <ardumanan@gmail.com>
2024-10-24 20:47:27 +0530

    [backend] Create route to get user details

--- backend/handlers/user.js
+++ backend/handlers/user.js
@@ -1,9 +1,56 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 const userDataHandler = (req, res) => {
-    res.status(200).json({
+    res.json({
         success: true,
         message: "User found",
         data: req.user,
     });
 };
 
-export { userDataHandler };
+const getUserByIdHandler = async (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        return res.status(400).json({
+            success: false,
+            message: "User Id is compulsory",
+            data: null,
+        });
+    }
+    const user = await prisma.user.findUnique({
+        where: {
+            id,
+        },
+        select: {
+            name: true,
+            points: true,
+            submissions: {
+                select: {
+                    id: true,
+                    language: true,
+                    problemStatementId: true,
+                    success: true,
+                    time: true,
+                },
+            },
+        },
+    });
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found",
+            data: null,
+        });
+    }
+    res.json({
+        success: true,
+        message: "User found",
+        data: {
+            user,
+        },
+    });
+};
+
+export { userDataHandler, getUserByIdHandler };
--- backend/router/user.js
+++ backend/router/user.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { userDataHandler } from "../handlers/user.js";
+import { userDataHandler, getUserByIdHandler } from "../handlers/user.js";
 
 import { checkAuth } from "../middlewares/auth.js";
 
 var router = Router();
 
 router.get("/", checkAuth, userDataHandler);
+router.get("/:id", checkAuth, getUserByIdHandler)
 
 export default router;
