b44aeb49806cc02cabfbf8bf9aff8f8c1d8b3f1a
Manan Gandhi <ardumanan@gmail.com>
2025-11-18 10:41:49 +0530

    [backend] Add setup step to ensure execution environment settings

--- backend/utils/docker.js
+++ backend/utils/docker.js
@@ -1,5 +1,8 @@
 import dockerode from "dockerode";
 
+const networkName = "no-internet";
+var useNetwork = true;
+
 const docker = dockerode();
 
 const filenames = {
@@ -11,6 +14,42 @@ const filenames = {
 
 const cleanStr = (str) => {
     return str.replaceAll('"', '\\"');
+};
+
+const setupExecutionEngine = async () => {
+    const allNetworks = await docker.listNetworks();
+    const networkExists = allNetworks.some(
+        (network) => network.Name === networkName
+    );
+    if (!networkExists) {
+        console.log("Creating Docker network for code execution...");
+        try {
+            await docker.createNetwork({ Name: networkName, Internal: true });
+        } catch (error) {
+            console.error("Error creating Docker network:", error);
+            console.error(
+                "Proceeding without network isolation for code execution."
+            );
+            useNetwork = false;
+        }
+    }
+    const allImages = await docker.listImages({ all: true });
+    const executionImageExists = allImages.some((container) =>
+        container.RepoTags.includes(process.env.CODE_RUNNER_CONTAINER)
+    );
+    if (!executionImageExists) {
+        console.log("Pulling code runner Docker image...");
+        try {
+            await docker.pull(process.env.CODE_RUNNER_CONTAINER);
+        } catch (error) {
+            console.error("Error pulling Docker image:", error);
+            console.error(
+                `Please run 'docker pull ${process.env.CODE_RUNNER_CONTAINER}' manually.`
+            );
+            process.exit(1);
+        }
+    }
+    console.log("Execution engine setup complete.");
 };
 
 const getExecutionCommand = (language, code, input) => {
@@ -31,16 +70,20 @@ const getExecutionCommand = (language, code, input) => {
 };
 
 const createDockerContainer = async (language, code, input) => {
-    return await docker.createContainer({
-        Image: process.env.CODE_RUNNER_CONTAINER,
-        Cmd: getExecutionCommand(language, code, input),
-        Tty: true,
-        HostConfig: {
-            NetworkMode: "online-ide_no-internet",
-            Memory: 128 * 1024 * 1024,
-            PidsLimit: 64,
-        },
-    });
+    try {
+        return await docker.createContainer({
+            Image: process.env.CODE_RUNNER_CONTAINER,
+            Cmd: getExecutionCommand(language, code, input),
+            Tty: true,
+            HostConfig: {
+                NetworkMode: useNetwork ? networkName : undefined,
+                Memory: 128 * 1024 * 1024,
+                PidsLimit: 64,
+            },
+        });
+    } catch (error) {
+        console.error("Error creating Docker container:", error);
+    }
 };
 
-export { createDockerContainer };
+export { setupExecutionEngine, createDockerContainer };
